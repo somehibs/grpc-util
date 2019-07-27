f604658691eed933dedd69c3c9af329b9732d55b
hibs@circuitco.de <hibs@circuitco.de>
2019-07-27 13:34:14 +0100

    forgot to commit interceptor updates

--- interceptors.go
+++ interceptors.go
@@ -5,11 +5,19 @@ import (
 	"time"
 
 	"golang.org/x/net/context"
+	"google.golang.org/grpc/metadata"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
 )
+
+type AuthProvider interface {
+	Check(string, map[string][]string) error
+}
+
+var NoAuthMethods = map[string]bool{}
+var AuthChecker AuthProvider
 
 func LogInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	p, _ := peer.FromContext(ctx)
@@ -26,4 +34,18 @@ func LogInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServer
 		fmt.Printf("%s %s %s %s\n", ts, addr, info.FullMethod, e)
 	}
 	return i, e
+}
+
+func AuthenticationInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	if NoAuthMethods[info.FullMethod] == true {
+		return handler(ctx, req)
+	}
+	p, _ := peer.FromContext(ctx)
+	peerAddr := p.Addr.String()
+	data, _ := metadata.FromIncomingContext(ctx)
+	err := AuthChecker.Check(peerAddr, data)
+	if err != nil {
+		return nil, err
+	}
+	return handler(ctx, req)
 }
